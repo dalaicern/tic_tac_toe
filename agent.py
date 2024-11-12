86e9c299ec0807ae42c5aef8a52c01061024a12b
Dalaitseren <dalai.num@gmail.com>
2024-11-12 14:40:58 +0800

    machine learning working

--- agent.py
+++ agent.py
@@ -1,164 +1,180 @@
-from abc import ABC, abstractmethod
-import os
 import pickle
-import collections
 import numpy as np
-import random
 
+BOARD_ROWS = 3
+BOARD_COLS = 3
 
-def getStateKey(board):
-        key = ''
+class State:
+    def __init__(self, p1, p2):
+        self.board = np.zeros((BOARD_ROWS, BOARD_COLS))
+        self.p1 = p1
+        self.p2 = p2
+        self.isEnd = False
+        self.boardHash = None
+        self.playerSymbol = 1
+    
+    def getHash(self):
+        self.boardHash = str(self.board.reshape(BOARD_COLS*BOARD_ROWS))
+        return self.boardHash
+    
+    def winner(self):
+        for i in range(BOARD_ROWS):
+            if sum(self.board[i, :]) == 3:
+                self.isEnd = True
+                return 1
+            if sum(self.board[i, :]) == -3:
+                self.isEnd = True
+                return -1
         
-        for row in board:
-            for item in row:
-                key += str(item)
-
-        return key
-
-
-class Agent(ABC):
-    def __init__ (self, alpha , gamma, eps, eps_decay):
-
-        self.alpha = alpha
-        self.gamma = gamma
-        self.eps = eps
-        self.eps_decay = eps_decay
-
-        self.actions = []
-
-        for i in range(3):
-            for j in range(3):
-                self.actions.append((i,j))
+        for i in range(BOARD_COLS):
+            if sum(self.board[:, i]) == 3:
+                self.isEnd = True
+                return 1
+            if sum(self.board[:, i]) == -3:
+                self.isEnd = True
+                return -1
+        
+        diag_sum1 = sum([self.board[i, i] for i in range(BOARD_COLS)])
+        diag_sum2 = sum([self.board[i, BOARD_COLS-i-1] for i in range(BOARD_COLS)])
+        diag_sum = max(diag_sum1, diag_sum2)
+        if diag_sum == 3:
+            self.isEnd = True
+            return 1
+        if diag_sum == -3:
+            self.isEnd = True
+            return -1
+        
+        
+        if len(self.availablePositions()) == 0:
+            self.isEnd = True
+            return 0
        
-        self.Q = {}
-        
-        self.rewards = []
-
-
-    @abstractmethod
-    def act(self, s):
-        pass
+        self.isEnd = False
+        return None
     
-
-    @abstractmethod
-    def update(self, s, prev_s, a, prev_a, r):
-        pass
-
-
-class Learner(Agent):
-
-    def __init__(self, alpha, gamma, eps, eps_decay=0.):
-        super().__init__(alpha, gamma, eps, eps_decay)
-
-    def act(self, s):
-        possible_actions = [a for a in self.actions if s[a[0]*3 + a[1]] == '-']
-
-        if len(possible_actions) == 0:
-                return -1 , -1
+    def availablePositions(self):
+        positions = []
+        for i in range(BOARD_ROWS):
+            for j in range(BOARD_COLS):
+                if self.board[i, j] == 0:
+                    positions.append((i, j))
+        return positions
+    
+    def updateState(self, position):
+        self.board[position] = self.playerSymbol
         
-
-        if random.random() < self.eps or s not in self.Q:
-
-            action = possible_actions[random.randint(0,len(possible_actions)-1)]
+        self.playerSymbol = -1 if self.playerSymbol == 1 else 1
+    
+    
+    def giveReward(self):
+        result = self.winner()
+        
+        if result == 1:
+            self.p1.feedReward(1)
+            self.p2.feedReward(0)
+        elif result == -1:
+            self.p1.feedReward(0)
+            self.p2.feedReward(1)
         else:
-
-            values = self.Q[s]
-
-
-            empty_q_values = [values[cell[0], cell[1]] for cell in possible_actions]     
-            max_q_value = max(empty_q_values)                                         
-            max_q_indices = [i for i in range(len(possible_actions)) if empty_q_values[i] == max_q_value] 
-            max_q_index = random.choice(max_q_indices)                             
-            action = tuple(possible_actions[max_q_index]) 
-
-
-        self.eps *= (1.-self.eps_decay)
-
-        return action
-
-
-
-    def update(self, state, next_state, action, reward):
-
-        q_values = self.Q.get(state, np.zeros((3, 3)))
-
-        next_q_values = self.Q.get(next_state, np.zeros((3, 3)))
-        max_next_q_value = np.max(next_q_values)
-
-        q_values[action[0], action[1]] += self.alpha * (reward + self.gamma * max_next_q_value - q_values[action[0], action[1]])
-
-        self.Q[state] = q_values
-
+            self.p1.feedReward(0.1)
+            self.p2.feedReward(0.5)
     
-    def is_game_over(self, board):
+    
+    def reset(self):
+        self.board = np.zeros((BOARD_ROWS, BOARD_COLS))
+        self.boardHash = None
+        self.isEnd = False
+        self.playerSymbol = 1
+    
+    def play(self, rounds=100):
+        for i in range(rounds):
+            if i%1000 == 0:
+                print("Rounds {}".format(i))
+            while not self.isEnd:
+                positions = self.availablePositions()
+                p1_action = self.p1.act(positions, self.board, self.playerSymbol)
+                self.updateState(p1_action)
+                board_hash = self.getHash()
+                self.p1.addState(board_hash)
+                
 
-        for row in board:
-            if len(set(row)) == 1 and row[0] != '-':      
-                return True, row[0]
+                win = self.winner()
+                if win is not None:
+                   
+                    self.giveReward()
+                    self.p1.reset()
+                    self.p2.reset()
+                    self.reset()
+                    break
 
-
-        for col in board.T:                               
-            if len(set(col)) == 1 and col[0] != '-':
-                return True, col[0]
-
-
-        
-        if len(set(board.diagonal())) == 1 and board[0, 0] != '-':           
-            return True, board[0, 0]
-        if len(set(np.fliplr(board).diagonal())) == 1 and board[0, 2] != '-': 
-            return True, board[0, 2]
-
-
-
-        if '-' not in board:
-            return True, 'draw'
-
-        return False, None
-    def learn(self):
-        num_draws = 0  # Counter for the number of draws
-        agent_wins = 0  # Counter for the number of wins by the agent
-        players = ['X', 'O']
-        num_episodes = 10000
-
-        for episode in range(num_episodes):
-            board = np.array([['-', '-', '-'],
-                            ['-', '-', '-'],
-                            ['-', '-', '-']])
-
-            current_player = random.choice(players)  # Randomly choose the current player
-            game_over = False
-
-            while not game_over:
-                action = self.act(getStateKey(board))  # Choose an action using the exploration rate
-
-                row, col = action
-                board[row, col] = current_player  # Update the board with the current player's move
-
-                game_over, winner = self.is_game_over(board)  # Check if the game is over and determine the winner
-
-                if game_over:
-                    if winner == current_player:  # Agent wins
-                        reward = 1
-                        agent_wins += 1
-                    elif winner == 'draw':  # Game ends in a draw
-                        reward = 0
-                        num_draws += 1
-                    else:  # Agent loses
-                        reward = -1
-                    self.update(getStateKey(board), getStateKey(board), action,  reward)  # Update the Q-table
                 else:
-                    current_player = players[(players.index(current_player) + 1) % 2]  # Switch to the next player
+                   
+                    positions = self.availablePositions()
+                    p2_action = self.p2.act(positions, self.board, self.playerSymbol)
+                    self.updateState(p2_action)
+                    board_hash = self.getHash()
+                    self.p2.addState(board_hash)
+                    
+                    win = self.winner()
+                    if win is not None:
+                        self.giveReward()
+                        self.p1.reset()
+                        self.p2.reset()
+                        self.reset()
+                        break
+    
+       
 
-                if not game_over:
-                    state = getStateKey(board)
-                    board[row, col] = current_player
-                    next_state = getStateKey(board)
-                    self.update(state, next_state, action, 0)  # Update the Q-table with the next state
+class Learner:
+    def __init__(self, name, exp_rate=0.3):
+        self.name = name
+        self.states = []
+        self.lr = 0.2
+        self.exp_rate = exp_rate
+        self.decay_gamma = 0.9
+        self.states_value = {}
+    
+    def getHash(self, board):
+        boardHash = str(board.reshape(3*3))
+        return boardHash
+    
+    def act(self, positions, current_board, symbol=-1):
+        if np.random.uniform(0, 1) <= self.exp_rate:
+            idx = np.random.choice(len(positions))
+            action = positions[idx]
+        else:
+            value_max = -999
+            for p in positions:
+                next_board = current_board.copy()
 
+                next_board[p] = symbol
+                next_boardHash = self.getHash(next_board)
+                value = 0 if self.states_value.get(next_boardHash) is None else self.states_value.get(next_boardHash)
+                if value >= value_max:
+                    value_max = value
+                    action = p
+        return action
+    
+    def addState(self, state):
+        self.states.append(state)
+    
+    def feedReward(self, reward):
+        for st in reversed(self.states):
+            if self.states_value.get(st) is None:
+                self.states_value[st] = 0
+            self.states_value[st] += self.lr*(self.decay_gamma*reward - self.states_value[st])
+            reward = self.states_value[st]
+            
+    def reset(self):
+        self.states = []
+        
+    def savePolicy(self):
+        fw = open('policy_' + str(self.name), 'wb')
+        pickle.dump(self.states_value, fw)
+        fw.close()
 
-        # Play multiple games between the trained agent and itself
-        agent_win_percentage = (agent_wins / num_episodes) * 100
-        draw_percentage = (num_draws / num_episodes) * 100
-
-        print("Agent win percentage: {:.2f}%".format(agent_win_percentage))
-        print("Draw percentage: {:.2f}%".format(draw_percentage))
+    def loadPolicy(self, file):
+        fr = open(file,'rb')
+        self.states_value = pickle.load(fr)
+        fr.close()
+    
--- main.py
+++ main.py
@@ -25,31 +25,28 @@ SCREEN.blit(BOARD, (64, 64))
 
 pygame.display.update()
 
-def getStateKey(board):
-        key = ''
-        
-        for row in board:
-            for item in row:
-                key += str(item)
-
-        return key
 
 class Game:
     def __init__(self):
-        self.board = np.array([['-','-','-'], ['-','-','-'], ['-','-','-']])
-        self.agent = Learner(alpha=0.5, gamma=0.9, eps=0.1)
+        self.board = np.zeros((BOARD_ROWS, BOARD_COLS))
         self.game_finished = False
 
+    def availablePositions(self):
+        positions = []
+        for i in range(BOARD_ROWS):
+            for j in range(BOARD_COLS):
+                if self.board[i, j] == 0:
+                    positions.append((i, j))
+        return positions
 
     def render_board(self, ximg, oimg):
         global graphical_board
         for i in range(3):
             for j in range(3):
-                if self.board[i][j] == 'X':
-                    # Create an X image and rect
+                if self.board[i][j] == 1:
                     graphical_board[i][j][0] = ximg
                     graphical_board[i][j][1] = ximg.get_rect(center=(j*300+150, i*300+150))
-                elif self.board[i][j] == 'O':
+                elif self.board[i][j] == -1:
                     graphical_board[i][j][0] = oimg
                     graphical_board[i][j][1] = oimg.get_rect(center=(j*300+150, i*300+150))
 
@@ -57,7 +54,7 @@ class Game:
     def add_XO(self, action, xo):
         global graphical_board
 
-        self.board[action[0]][action[1]] = xo
+        self.board[action[0]][action[1]] = 1 if xo == 'X' else -1
 
 
         self.render_board(X_IMG, O_IMG)
@@ -71,7 +68,7 @@ class Game:
     def check_win(self):
         winner = None
         for row in range(0, 3):
-            if((self.board[row][0] == self.board[row][1] == self.board[row][2]) and (self.board [row][0]  != '-')):
+            if abs(sum(self.board[row, :])) == 3:
                 winner = self.board[row][0]
                 for i in range(0, 3):
                     graphical_board[row][i][0] = pygame.image.load(f"assets/Winning {winner}.png")
@@ -80,7 +77,7 @@ class Game:
                 return winner
 
         for col in range(0, 3):
-            if((self.board[0][col] == self.board[1][col] == self.board[2][col]) and (self.board[0][col] != '-')):
+            if abs(sum(self.board[:, col])) == 3:
                 winner =  self.board[0][col]
                 for i in range(0, 3):
                     graphical_board[i][col][0] = pygame.image.load(f"assets/Winning {winner}.png")
@@ -88,7 +85,7 @@ class Game:
                 pygame.display.update()
                 return winner
     
-        if (self.board[0][0] == self.board[1][1] == self.board[2][2]) and (self.board[0][0] != '-'):
+        if (self.board[0][0] == self.board[1][1] == self.board[2][2]) and (self.board[0][0] != 0):
             winner =  self.board[0][0]
             graphical_board[0][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
             SCREEN.blit(graphical_board[0][0][0], graphical_board[0][0][1])
@@ -99,7 +96,7 @@ class Game:
             pygame.display.update()
             return winner
             
-        if (self.board[0][2] == self.board[1][1] == self.board[2][0]) and (self.board[0][2] != '-'):
+        if (self.board[0][2] == self.board[1][1] == self.board[2][0]) and (self.board[0][2] != 0):
             winner =  self.board[0][2]
             graphical_board[0][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
             SCREEN.blit(graphical_board[0][2][0], graphical_board[0][2][1])
@@ -113,13 +110,26 @@ class Game:
         if winner is None:
             for i in range(len(self.board)):
                 for j in range(len(self.board)):
-                    if self.board[i][j] != 'X' and self.board[i][j] != 'O':
+                    if self.board[i][j] != 1 and self.board[i][j] != -1:
                         return None
             return "DRAW"
         
 
     def play(self):
-        self.agent.learn()
+        # p1 = Learner("p1")
+        # p2 = Learner("p2")
+
+        # st = State(p1, p2)
+        # print("training...")
+        # st.play(50001)
+
+        p1 = Learner("p1")
+        p1.loadPolicy("policy_p1")
+        # print(p2.states_value)
+        positions = self.availablePositions()
+        action = p1.act(positions, self.board, 1)
+        print(action)
+        self.add_XO( action, 'X')
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -131,42 +141,32 @@ class Game:
                     x = round((current_pos[0]-65)/835*2)
                     y = round(current_pos[1]/835*2)
 
-                    if self.board[y][x] == '-':
-                        self.add_XO( (y, x), 'X')
+                    if self.board[y][x] == 0:
+                        self.add_XO((y, x), 'O')
 
-                        state = getStateKey(self.board)
-                        action = self.agent.act(state)
+                        winner = self.check_win()
+                        if winner is not None:
+                            print(winner)
+                            pygame.display.update()
+                            pygame.time.wait(3000)
+                            pygame.quit()
+                            sys.exit()
+                            
 
-                        
-                        if action[0] == -1:
-                            self.check_win()
-                        else:
-                            self.add_XO( action, 'O')
+                        positions = self.availablePositions()
 
+                        if(len(positions) > 0):
+                            action = p1.act(positions, self.board, 1)
+                            self.add_XO( action, 'X')
 
-                winner = self.check_win()
-                if winner is not None:
-                    if winner == 'O':  # Agent wins
-                        reward = 1
-                    elif winner == 'DRAW':  # Game ends in a draw
-                        reward = 0
-                    else:  # Agent loses
-                        reward = -1
-                    self.agent.update(getStateKey(self.board), getStateKey(self.board), action,  reward)
-                    
-                    global graphical_board
-                    self.board = [['-','-','-'], ['-','-','-'], ['-','-','-']]
-                    graphical_board = [[[None, None], [None, None], [None, None]], 
-                                        [[None, None], [None, None], [None, None]], 
-                                        [[None, None], [None, None], [None, None]]]
+                        winner = self.check_win()
+                        if winner is not None:
+                            print(winner)
+                            pygame.display.update()
+                            pygame.time.wait(3000)  
+                            pygame.quit()
+                            sys.exit()
 
-
-                    SCREEN.fill(BG_COLOR)
-                    SCREEN.blit(BOARD, (64, 64))
-
-                    self.game_finished = False
-            
-                
                 pygame.display.update()
 
 game = Game()
